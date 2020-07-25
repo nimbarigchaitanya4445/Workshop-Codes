2098c0722ee8161d4560d57dcd2452680e51165b
nimbarigchaitanya4445 <68779149+nimbarigchaitanya4445@users.noreply.github.com>
2020-07-25 19:05:59 +0530

    Created Fibonacci.py
    This files prints the Fibonacci series.

---
+++ Fibonacci.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+n = input('Enter the term ');
+n = int(n);
+a = 0;
+b = 1;
+for i in range(0,n) :
+    print(a);
+    summa =  b + a;
+    a = b;
+    b = summa;
+
+
+# In[ ]:
+
+
+
+
